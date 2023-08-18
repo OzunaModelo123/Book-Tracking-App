2dcf88f65a74c839703cbd2e1c5a63273cfeb264
Daniel Ozoani <57331891+OzunaModelo123@users.noreply.github.com>
2023-08-18 05:00:53 -0400

    Migrations
    i added data migrations

---
+++ Book_Track/Data/BookDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Book_Track.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Book_Track.Data
+{
+    public class BookDbContext
+    {
+        public DbSet<Book>? Books { get; set; }
+        public DbSet<Category>? Categories { get; set; }
+        public DbSet<CategoryType>? CategoryTypes { get; set; }
+    }
+}
